7ffc6eca5ab4ded1ddf41b467be27ce78b2e04c7
Michael <michelpaliz@hotmail.com>
2022-02-19 18:41:27 +0100

    changes for god sake

--- OOP/src/main/java/EjerciciosTema8/Ejercicio6/Bicicleta.java
+++ OOP/src/main/java/EjerciciosTema8/Ejercicio6/Bicicleta.java
@@ -1,28 +1,59 @@
 package EjerciciosTema8.Ejercicio6;
 
-import java.time.LocalDate;
 import java.util.GregorianCalendar;
 
 public class Bicicleta {
-    private int referencia;
-    private String marca;
-    private double peso;
-    private double dimensionRuedas;
-    private boolean motor;
-    private LocalDate fechaFabricacion;
-    private double precio;
-    private int numeroExistencias;
+    private final String referencia;
+    private final String marca;
+    private final double peso;
+    private final double dimensionRuedas;
+    private final boolean motor;
+    private final GregorianCalendar fechaFabricacion;
+    private final double precio;
+    private final int numeroExistencias;
 
-    public Bicicleta(int referencia, String marca, double peso, double dimensionRuedas, boolean motor,
-            int anyo, int mes, int dia, double precio, int numeroExistencias) {
+    public Bicicleta(String referencia, String marca, double peso, double dimensionRuedas, boolean motor,
+            GregorianCalendar fechaFabricacion, double precio, int numeroExistencias) {
         this.referencia = referencia;
         this.marca = marca;
         this.peso = peso;
         this.dimensionRuedas = dimensionRuedas;
         this.motor = motor;
-        this.fechaFabricacion = LocalDate.of(anyo, mes, dia);
+        this.fechaFabricacion = fechaFabricacion;
         this.precio = precio;
         this.numeroExistencias = numeroExistencias;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getDimensionRuedas() {
+        return dimensionRuedas;
+    }
+
+    public boolean isMotor() {
+        return motor;
+    }
+
+    public GregorianCalendar getFechaFabricacion() {
+        return fechaFabricacion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getNumeroExistencias() {
+        return numeroExistencias;
     }
 
     @Override
--- OOP/src/main/java/EjerciciosTema8/Ejercicio6/Tienda.java
+++ OOP/src/main/java/EjerciciosTema8/Ejercicio6/Tienda.java
@@ -1,38 +1,59 @@
 package EjerciciosTema8.Ejercicio6;
 
-import java.security.interfaces.RSAKey;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 import com.github.javafaker.Faker;
 
 public class Tienda {
-    private Scanner myInput = new Scanner(System.in);
     int userInt;
     String userStr;
     int baseDatos;
     boolean correct;
+    Scanner myInput = new Scanner(System.in);
 
     public void menu() {
         System.out.println("***GESTION DE BICICLETAS*****");
         do {
             System.out.println("1-Añadir bicicleta");
             System.out.println("2-Vender bicicleta");
-            System.out.println("3-Mostrar Stock");
+            System.out.println("3-Consultar bicileta");
+            System.out.println("4.Mostrar Stock");
             System.out.println("-------------------------");
             System.out.println("0-Salir");
             userInt = Integer.parseInt(myInput.nextLine());
-            correct = userInt > 0 && userInt <= 4;
+            correct = userInt <= 4;
+            if (!correct) {
+                System.out.println("Introduce un numero valido");
+            }
 
         } while (!correct);
 
         switch (userInt) {
+            case 0:
+                System.out.println("Hasta luego");
             case 1:
-
+                nuevaBicicleta();
             case 2:
+
+            case 3:
+
+            case 4:
 
         }
 
     }
+
+    public static CrearDatosPrueba(){
+
+
+    }
+
+    /**
+     * 
+     * @return clase Bicicleta
+     */
 
     public Bicicleta nuevaBicicleta() {
         System.out.println("***VAMOS AÑADIR UNA NUEVA BICICLETA********");
@@ -46,14 +67,45 @@ public class Tienda {
         Double sizeRuedas = Double.parseDouble(myInput.nextLine());
         System.out.println("TIENE MOTOR (SI/NO)");
         String motor = myInput.nextLine();
-        validarMotor()
-        // +Todo metodo para validar esta respuesta
+        validarMotor(motor);
         System.out.println("QUE PRECIO TIENE");
         double precion = Double.parseDouble(myInput.nextLine());
+        System.out.println("INDICA LA FECHA DE FABRICACION");
+        validarFecha();
         System.out.println("INDICA EL NUMERO DE EXISTENCIAS QUE TIENEN DE ESA REFERENCIAS");
-        // +Todo metodo para indicar el numero de referencias que hayas
+        // +Todo metodo para indicar el numero de referencias que hay
+    }
+
+    /**
+     * 
+     * @return fecha del usuario
+     */
+    public String validarFecha() {
+        int anyo;
+        int mes;
+        int dia;
+        String fechaStr = "";
+        GregorianCalendar fecha = new GregorianCalendar();
+        do {
+            System.out.println("INSERTA EL AÑO ");
+            anyo = fecha.get(GregorianCalendar.YEAR);
+            mes = fecha.get(GregorianCalendar.MONTH);
+            dia = fecha.get(GregorianCalendar.DAY_OF_MONTH);
+            correct = anyo <= fecha.get(Calendar.YEAR);
+            if (!correct) {
+                System.out.println("La fecha de fabricacion no puede ser superior a la actual");
+            }
+        } while (!correct);
+        fechaStr = dia + "/" + mes + "/" + anyo;
+        return fechaStr;
 
     }
+
+    /**
+     * 
+     * @param motor
+     * @return una respuesta valida como si o no
+     */
 
     public String validarMotor(String motor) {
 
