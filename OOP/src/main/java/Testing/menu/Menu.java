550309d3448bc0316246700a954df0d46445b59d
Michael <michelpaliz@hotmail.com>
2022-03-30 17:59:43 +0200

    changes

--- OOP/src/main/java/Testing/menu/Menu.java
+++ OOP/src/main/java/Testing/menu/Menu.java
@@ -14,10 +14,12 @@ public class Menu {
         String con = "";
         String var = "";
         System.out.printf("*****%S********\n", title);
-        for (int i = 0; i < sentence.length; i++) {
-            var = sentence[i];
+
+        for (int i = 0; i < 1; i++) {
+            // var = sentence[i];
+            var = sentence[0] + "\n";
             // sentence[i] = String.format("%d %S\n", (i + 1), sentence[i]);
-            con = String.format("%d %S\n", (i + 1), var);
+            con = String.format("%d, %S ", (i + 1), var);
             System.out.print(con);
 
         }
