e7a70b2fe8c81468da253f47d8f38c7245830d18
Michael <michelpaliz@hotmail.com>
2022-04-19 23:55:44 +0200

    changes

---
+++ OOP/src/main/java/Tema11/Propios/ejercicio05/models/inventary.java
@@ -0,0 +1,33 @@
+package Tema11.Propios.ejercicio05.models;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+
+
+public class inventary extends JPanel implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+
+
+
+}
