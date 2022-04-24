fc9b69c970ef4e015490efb65af290a99ddc0f01
MichaelUbuntu <michelpaliz@hotmail.com>
2022-04-24 13:27:58 +0200

    changes

--- OOP/src/main/java/Testing/array/Array.java
+++ OOP/src/main/java/Testing/array/Array.java
@@ -2,6 +2,8 @@ package Testing.array;
 
 import java.util.Arrays;
 
+import Util.Lib;
+
 public class Array {
 
     public Array() {
@@ -11,6 +13,10 @@ public class Array {
         int[] newArr = mergeArray(original, implement);
         int[] arr = mergeArray(newArr, implement);
         System.out.println(Arrays.toString(arr));
+        String[] arrStr = { "hello", "bye", "how are you" };
+        System.out.println(getRndValueArr(arrStr));
+        // String result = getRndValueArr(arrStr);
+        // System.out.println(result);
 
     }
 
@@ -30,6 +36,15 @@ public class Array {
             newArr[original.length + i] = implement[i];
         }
         return newArr;
+    }
+
+    public <T> T getRndValueArr(T arr[]) {
+        // Store the index in a variable
+        // int rnd = (int) (Math.random() * arr.length);
+        int rnd = (Lib.r).nextInt(arr.length);
+        // Then access it using another variable
+        T random = (arr[rnd]);
+        return random;
     }
 
 }
--- OOP/src/main/java/Util/MyArr.java
+++ OOP/src/main/java/Util/MyArr.java
@@ -24,7 +24,7 @@ public class MyArr {
      * @param <T>
      * @param original
      * @param implement
-     * @return
+     * @return the new implement obj array
      */
     public <T> T[] mergeArray(T original[], T implement[]) {
         int newLength = original.length + implement.length;
@@ -33,6 +33,21 @@ public class MyArr {
             newArr[original.length + i] = implement[i];
         }
         return newArr;
+    }
+
+    /**
+     * 
+     * @param <T>
+     * @param arr
+     * @return the random obj selected
+     */
+    public <T> T getRndValueArr(T arr[]) {
+        // Store the index in a variable
+        // int rnd = (int) (Math.random() * arr.length);
+        int rnd = (Lib.r).nextInt(arr.length);
+        // Then access it using another variable
+        T random = (arr[rnd]);
+        return random;
     }
 
 }
