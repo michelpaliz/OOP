d89fb4d4d5b19cd12b2dcaafab629078e553ea0d
MichaelUbuntu <michelpaliz@hotmail.com>
2022-04-26 20:11:36 +0200

    changes

---
+++ OOP/src/main/java/Tema11/Propios/ejercicio06/Format.java
@@ -0,0 +1,5 @@
+package Tema11.Propios.ejercicio06;
+
+public enum Format {
+    CD, DVD, BLUE_RAY, FOLDER;
+}
---
+++ OOP/src/main/java/Tema11/Propios/ejercicio06/Multemedia.java
@@ -0,0 +1,28 @@
+package Tema11.Propios.ejercicio06;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class Multemedia {
+    // to store info of our objects
+    private String title;
+    private String name;
+    private Format format;
+    private GregorianCalendar launchDate;
+
+    public Multemedia() {
+        this.title = "My book";
+        this.name = "MIchael";
+        this.format = format.BLUE_RAY;
+        this.launchDate = new GregorianCalendar(TimeZone.getTimeZone("GMT+5:30"));
+    }
+
+    public Multemedia(String title, String name, Format format, GregorianCalendar launchDate) {
+        this.title = title;
+        this.name = name;
+        this.format = format;
+        this.launchDate = launchDate;
+    }
+
+}
