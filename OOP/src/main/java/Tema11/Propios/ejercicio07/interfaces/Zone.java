ced9f030603acbdb572eeca0d05639a6d26b4abb
MichaelUbuntu <michelpaliz@hotmail.com>
2022-05-02 20:43:51 +0200

    changes

--- OOP/src/main/java/Tema11/Propios/ejercicio07/interfaces/Zone.java
+++ OOP/src/main/java/Tema11/Propios/ejercicio07/interfaces/Zone.java
@@ -6,14 +6,16 @@ import Tema11.Propios.ejercicio07.models.ticket.Influence;
 public interface Zone {
 
     public default double zone(Influence inf, Match m) {
-
+        double result;
         switch (inf) {
             case LOW_INFLUENCE:
-                return m.getBasePrice() * 0.75;
+                return m.getBasePrice();
             case MEDIUM_INFLUENCE:
-                return m.getBasePrice() * 0.75;
+                result = m.getBasePrice() * 0.75;
+                return m.getBasePrice() + result;
             case HIGH_INFLUENCE:
-                return m.getBasePrice() * 130;
+                result = m.getBasePrice() * 1.3;
+                return m.getBasePrice() + result;
         }
         return 0;
 
