20038f4bab6eb02117341e500a3045a82378c0f6
MichaelUbuntu <michelpaliz@hotmail.com>
2022-06-24 20:16:01 +0200

    changes

---
+++ OOP/src/main/java/Examen/Models/Venta.java
@@ -0,0 +1,27 @@
+package Examen.Models;
+
+import java.util.GregorianCalendar;
+
+public class Venta {
+    private static int NUM_VENTA = 0;
+
+    private final int id;
+    private final Mueble mueble;
+    private final GregorianCalendar diaCompra;
+
+    public Venta(Mueble mueble, GregorianCalendar diaCompra) {
+        this.id = ++NUM_VENTA;
+        this.mueble = mueble;
+        this.diaCompra = diaCompra;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public GregorianCalendar getDiaCompra() {
+        return diaCompra;
+    }
+
+
+}
